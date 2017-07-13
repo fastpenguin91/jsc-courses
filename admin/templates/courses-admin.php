da111a388a57299d9859b6438d3682a0b4945327
John Curry <john.steven.curry@gmail.com>
2017-07-13 12:28:36 -0700

    need refactoring. about to add javascript to add new lessons to section via javascript possibly

--- admin/templates/courses-admin.php
+++ admin/templates/courses-admin.php
@@ -20,19 +20,37 @@
     <input type="submit" value="submit">
 </form>
 
+<h1>You are editing the PHP Basics Section</h1>
+
 <?php
-$args = array(
-	'post_type' => 'lessons'
-);
+global $wpdb;
 
-$lessons = get_posts( $args );
+$section_lessons = $wpdb->get_results("SELECT wp_posts.ID, wp_posts.post_title, wp_jsc_courses_sections.section_id, wp_jsc_courses_sections_lessons.position
+FROM wp_posts
+INNER JOIN wp_jsc_courses_sections_lessons ON wp_posts.ID=wp_jsc_courses_sections_lessons.lessons_id
+INNER JOIN wp_jsc_courses_sections ON wp_jsc_courses_sections_lessons.sections_id=wp_jsc_courses_sections.section_id
+WHERE post_type='lessons' AND wp_jsc_courses_sections.section_id = 1
+ORDER BY wp_jsc_courses_sections_lessons.position");
 
-for ( $i = 0; $i < count($lessons); $i++ ) {
-	?>
-<div class="admin-lesson">
-	<span class="admin-lesson-title"><?php echo $lessons[$i]->post_title;?></span>
-</div>
+//die(var_dump($section_lessons));
+
+$count_lessons = count($section_lessons);
+//TODO Create form with a position and the title of each lesson
+//TODO learn javascript and dragging/dropping
+
+?>
+<form id="sectionForm">
+<?php
+
+//echo "<input type='hidden' name='section_id' value='" . $section_lessons[0]->section_id . "'";
+for ($i = 0; $i < $count_lessons; $i++) {
+    echo "<div class='admin-lesson'><strong>" . $section_lessons[$i]->post_title . "</strong>";
+    echo "<input type='number' name='post_" . $section_lessons[$i]->ID ."' value='". $section_lessons[$i]->position . "' ></div>";
+}
+?>
+
+</form>
+
 <?php
 //    TODO Create list of lesson ids to store in database as a "section"
     // TODO Access & display these lessons via shortcode
-}
